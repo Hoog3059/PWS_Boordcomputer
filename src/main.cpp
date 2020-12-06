d1bce1a004ca363e5f10020369b7e4ad46d3d800
Timo Hoogenbosch <timo.hoogenbosch@gmail.com>
2020-12-06 17:06:14 +0100

    - Move old code to /old - Remove old code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
-//#define ACCELGYRO
 //#define ACCELGYRO_CALIBRATE
-//#define DMP_TEST
-//#define ALTIMETER
-//#define ALTIMETER_TEST
 #define FINAL
-//#define TEST
 
-#ifdef ACCELGYRO
-#include "accelgyro.h"
-#elif defined(ACCELGYRO_CALIBRATE)
+#ifdef ACCELGYRO_CALIBRATE
 #include "accelgyro_calibrate.h"
-#elif defined(DMP_TEST)
-#include "dmp_test.h"
-#elif defined(ALTIMETER)
-#include "altimeter.h"
-#elif defined(ALTIMETER_TEST)
-#include "altimeter_test.h"
 #elif defined(FINAL)
 #include "final.h"
-#elif defined(TEST)
-#include "test.h"
 #endif
